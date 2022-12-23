3873bb6e4d2761525a0501834806eacc79a64cff
Николай Балыков <balykovnikolay13@gmail.com>
2022-12-24 04:03:37 +0700

    Add urls and views functions, update setting.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
